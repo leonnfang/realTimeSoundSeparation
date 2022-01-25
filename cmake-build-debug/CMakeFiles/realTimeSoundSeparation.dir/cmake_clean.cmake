84717584fb69a65f681ee81a926f714c4037e23b
leonfang <fang@cooper.edu>
2022-01-25 13:00:06 -0500

    first init

---
+++ cmake-build-debug/CMakeFiles/realTimeSoundSeparation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realTimeSoundSeparation.dir/main.cpp.o"
+  "realTimeSoundSeparation"
+  "realTimeSoundSeparation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realTimeSoundSeparation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
