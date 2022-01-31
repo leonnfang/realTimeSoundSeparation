bdcc1575e7eb8548c0608b2cda88216addb8057f
leonfang <fang@cooper.edu>
2022-01-30 21:55:56 -0500

    remove portaudio dirs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+//PaError err = Pa_Initialize();
+//const char* error_message = Pa_GetErrorText(err);
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // terminate the pa
+//    err = Pa_Terminate();
+//    if( err != paNoError )
+//        printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
     return 0;
 }
