ae95b8a6811f1ff3a649cbb82d1f03604d0e354f
leonfang <fang@cooper.edu>
2022-02-01 10:57:12 -0500

    updates CMakeLists.txt

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/paex_record.dir/DependInfo.cmake"
   "CMakeFiles/realTimeSoundSeparation.dir/DependInfo.cmake"
   )
--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+
 #include <portaudio.h>
+
+
+#define WRITE_TO_FILE   (0)
+/* Select sample format. */
+#if 1
+#define PA_SAMPLE_TYPE  paFloat32
+typedef float SAMPLE;
+#define SAMPLE_SILENCE  (0.0f)
+#define PRINTF_S_FORMAT "%.8f"
+#elif 1
+#define PA_SAMPLE_TYPE  paInt16
+typedef short SAMPLE;
+#define SAMPLE_SILENCE  (0)
+#define PRINTF_S_FORMAT "%d"
+#elif 0
+#define PA_SAMPLE_TYPE  paInt8
+typedef char SAMPLE;
+#define SAMPLE_SILENCE  (0)
+#define PRINTF_S_FORMAT "%d"
+#else
+#define PA_SAMPLE_TYPE  paUInt8
+typedef unsigned char SAMPLE;
+#define SAMPLE_SILENCE  (128)
+#define PRINTF_S_FORMAT "%d"
+#endif
+
+typedef struct
+{
+    int          frameIndex;  /* Index into sample array. */
+    int          maxFrameIndex;
+    SAMPLE      *recordedSamples;
+}
+        paTestData;
+
+
 PaError err = Pa_Initialize();
 const char* error_message = Pa_GetErrorText(err);
 int main() {
@@ -9,5 +45,10 @@ int main() {
     err = Pa_Terminate();
     if( err != paNoError )
         printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
+    else{
+        std::cout << "no error" << std::endl;
+        std::cout << Pa_GetErrorText(err) << std::endl;
+
+    }
     return 0;
 }
